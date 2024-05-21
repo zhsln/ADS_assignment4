ce16152136c72270f524269480927aa18c829e1c
zhsln <153474492+zhsln@users.noreply.github.com>
2024-05-21 23:53:40 +0500

    Added Vertex class.

---
+++ src/Vertex.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+
+
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+}
