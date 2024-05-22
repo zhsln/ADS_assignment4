48453f1c3e60d8008b2ff25203955d229b3b1a8f
zhsln <153474492+zhsln@users.noreply.github.com>
2024-05-23 00:43:20 +0500

    Added JavaDoc comments

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BreadthFirstSearch"},{"p":"<Unnamed>","l":"DijkstraSearch"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Search"},{"p":"<Unnamed>","l":"Vertex"},{"p":"<Unnamed>","l":"WeightedGraph"}];updateSearchResults();
